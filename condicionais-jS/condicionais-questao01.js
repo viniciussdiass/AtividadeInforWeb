4cce8c697fc6e01b553fbdb34cf3979c6ceb9a5b
viniciussdiass <viniciuclash2017@gmail.com>
2024-10-29 09:20:01 -0300

    Update condicionais-questao01.js

--- condicionais-jS/condicionais-questao01.js
+++ condicionais-jS/condicionais-questao01.js
@@ -1,15 +1,14 @@
 def comparar_numeros(num1, num2):
-    if num1 % 2 != 0 or num2 % 2 != 0:  # Se um dos números for ímpar
+    if num1 % 2 != 0 or num2 % 2 != 0:  
         if num1 % 2 != 0 and num2 % 2 == 0:
             maior = num1
         elif num2 % 2 != 0 and num1 % 2 == 0:
             maior = num2
         else:
             maior = max(num1, num2)
-    else:  # Ambos são pares
+    else: 
         maior = max(num1, num2)
     
     print(f"O maior número é: {maior}")
 
-# Exemplo de uso
 comparar_numeros(4, 5)
